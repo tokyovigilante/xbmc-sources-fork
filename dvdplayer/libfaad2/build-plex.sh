ab076e1a3a4dc32116044fb2c2cb9a1137617981
tokyovigilante <ryanwalklin@gmail.com>
2008-07-07 15:44:57 +1200

    upgraded libfaad2

--- dvdplayer/libfaad2/build-plex.sh
+++ dvdplayer/libfaad2/build-plex.sh
@@ -6,11 +6,9 @@ if [ $PLATFORM != "x86_64" ]; then
    PLATFORM="osx"
 fi
 
-make clean &&
-
 autoreconf -vif &&
-ARCHFLAGS="./configure" ./configure --with-mp4v2 --with-pic &&
+./configure --with-mp4v2 --with-pic &&
 make &&
 
 echo wrapping libfaad &&
-gcc -shared -fpic --soname -dynamiclib -flat_namespace -undefined suppress -o libfaad2-$PLATFORM.so -rdynamic libfaad/*.o 
+gcc -shared -fpic --soname -dynamiclib -flat_namespace -undefined 	suppress -o libfaad2-$PLATFORM.so -rdynamic libfaad/*.o 
