ebba1ce969303b98e79896d737049072b3525829
Ryan Walklin <ryan@Firebert.(none)>
2009-03-09 16:01:43 +1300

    Remove portaudio and libnemesi, upgrade libfaad2 and use static lib

--- dvdplayer/libfaad2/build-plex.sh
+++ dvdplayer/libfaad2/build-plex.sh
@@ -9,6 +9,3 @@ fi
 autoreconf -vif &&
 ./configure --with-mp4v2 --with-pic &&
 make &&
-
-echo wrapping libfaad &&
-gcc -shared -fpic --soname -dynamiclib -flat_namespace -undefined 	suppress -o libfaad2-$PLATFORM.so -rdynamic libfaad/*.o 
