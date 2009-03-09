ac3c6ef8c567f1eeb750ce6e74c63c2d53fcde11
Ryan Walklin <ryan@Firebert.(none)>
2009-03-10 09:33:58 +1300

    Upgrade libass

--- dvdplayer/libfaad2/build-plex.sh
+++ dvdplayer/libfaad2/build-plex.sh
@@ -1,11 +1,5 @@
 #!/bin/bash 
-# Requires libtool
-
-PLATFORM=`uname -m`
-if [ $PLATFORM != "x86_64" ]; then
-   PLATFORM="osx"
-fi
 
 autoreconf -vif &&
-./configure --with-mp4v2 --with-pic &&
-make &&
+./configure --with-pic &&
+make
